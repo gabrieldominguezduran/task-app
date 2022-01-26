9d2bc82561929915a90b8a877028a5a165e41b28
gabrieldominguezduran <gabrieldominguezduran@gmail.com>
2022-01-26 18:12:17 +0100

    Set tasks array

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,27 @@ import React, { useState } from 'react';
 import './App.css';
 
 import InputForm from './components/InputForm';
+import { Task } from './model';
+
 
 const App = () =>{
   const [task, setTask] = useState<string>("")
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  const handleAddTask = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    if (task) {
+      setTasks([...tasks, {id: Date.now(), task, isDone: false}])
+      setTask("")
+    }
+  }
+  console.log(tasks);
   
   return (
     <div className="App">
       <span className="heading">Tasks</span>
-        <InputForm task={task} setTask={setTask}/>
+        <InputForm task={task} setTask={setTask} handleAddTask={handleAddTask}  />
     </div>
   );
 }
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -3,11 +3,12 @@ import './styles.css'
 
 interface Props{
   task: string;
-  setTask: React.Dispatch<React.SetStateAction<string>>
+  setTask: React.Dispatch<React.SetStateAction<string>>;
+  handleAddTask: (e: React.FormEvent) => void;
 }
 
-const InputForm = ({task, setTask}: Props) => {
-  return <form className='form'>
+const InputForm = ({task, setTask, handleAddTask}: Props) => {
+  return <form className='form' onSubmit={handleAddTask}>
     <input type="input" className='form__input'  placeholder='Enter a task name' value={task} onChange={(e) => setTask(e.target.value) }/>
     <button className='form__btn' type='submit'>Go</button>
   </form>
